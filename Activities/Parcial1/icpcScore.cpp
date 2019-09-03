d34e34d47f1fe953ead1450e25850fd7ba6099cd
Jaime Hisao <jaimhisao2011@gmail.com>
2019-09-02 19:17:26 -0500

    Finished icpc

--- Activities/Parcial1/icpcScore.cpp
+++ Activities/Parcial1/icpcScore.cpp
@@ -1,41 +1,73 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-class Teams{
-    public:
-        string name;
-        int numberOfSubmissions;
-        int score;
+struct Teams{
+        string name = "";
+        int numberOfAccepted = 0, score = 0;
+        int arr[10] = {0,0,0,0,0,0,0,0,0,0};
 };
 
-Teams::Teams(){
-    name = "";
-    numberOfSubmissions = 0;
-    score = 0;
+
+bool compare(Teams t2, Teams t1){
+        return (t1.numberOfAccepted < t2.numberOfAccepted || (t1.numberOfAccepted == t2.numberOfAccepted && t1.score > t2.score));
 }
 
-
 int main(){
-
     int numberOfTeams, numberOfProblems;
-    map<string,Teams> teamMap;
+    vector<Teams> teamVector;
     cin>>numberOfTeams>>numberOfProblems;
 
-    Teams tmpTeam;
     for(int i = 0; i < numberOfTeams; i++){
         Teams tmpTeam;
         cin>>tmpTeam.name;
-        teamMap["tmpTeam.name"] = tmpTeam;
+        teamVector.push_back(tmpTeam);
     }
 
     int numberOfSubmssionsSent;
+    cin>>numberOfSubmssionsSent;
 
     for(int i = 0; i < numberOfSubmssionsSent; i++){
+        //Team name, Problem, Time and Accepted or Wrong (A, W)
+        int time;
+        string name, status;
+        char problem;
+        cin>>name>>problem>>time>>status;
+
+        for(int j = 0; j < teamVector.size(); j++){
+            if(teamVector.at(j).name == name){
+                 if(status == "A"){
+                    if(teamVector.at(j).arr[problem - 'A'] != 0){
+                        teamVector.at(j).score += time + (20 * teamVector.at(j).arr[problem - 'A']);
+                        teamVector.at(j).numberOfAccepted++;  
+                    }else{
+                        teamVector.at(j).score += time;
+                        teamVector.at(j).numberOfAccepted++;  
+                    }
+                }else{
+                    teamVector.at(j).arr[problem-'A']++;
+        }
+            }
+        }
 
     }
 
+    sort(teamVector.begin(), teamVector.end(), compare);
 
-}
+    for(int i = 0; i < teamVector.size(); i++){
+        cout<<i+1<<" "<<"-"<<" ";
+        cout<<teamVector.at(i).name<<" "<<teamVector.at(i).numberOfAccepted<<" ";
+        if(teamVector.at(i).score == 0){
+            cout<<"-"<<endl;
+        }else{
+            cout<<teamVector.at(i).score<<endl;
+        }
+    }
+
+    return 0;
+}   
+
+
